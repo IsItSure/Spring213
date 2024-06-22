6a530a3760456d5cb6918a6679a28c1599a3c55f
IsItSure <noobukgames@gmail.com>
2024-06-22 23:32:19 +0300

    done

---
+++ src/main/java/App.java
@@ -0,0 +1,19 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld helloBean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld helloBean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(helloBean1 == helloBean2);
+
+        Cat catBean1 = (Cat) applicationContext.getBean("cat");
+        Cat catBean2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean1 == catBean2);
+    }
+}
